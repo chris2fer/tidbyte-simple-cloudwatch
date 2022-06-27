e1890261b486ea640ab9854b6ad60ff044ffb6b0
Christopher Dearie <cdearie@bloomberg.net>
2022-06-27 16:02:11 -0700

    Testing

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Christopher"
-# pixlet render main.star
-# pixlet push --api-token $API_TOKEN $DEVICE_ID main.webp
+
+pixlet render main.star
+pixlet push --api-token $API_TOKEN $DEVICE_ID main.webp
